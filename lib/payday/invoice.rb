2e53d4defdc41a06329dca675e72de5fafae96d8
yangweiqi <phpcn@hotmail.com>
2019-05-16 00:14:36 +0800

    Update invoice.rb

--- lib/payday/invoice.rb
+++ lib/payday/invoice.rb
@@ -27,11 +27,15 @@ module Payday
     # The tax rate that we're applying, as a BigDecimal
     def tax_rate=(value)
       @tax_rate = BigDecimal.new(value.to_s)
+    rescue ArgumentError
+      @tax_rate = 0.0
     end
 
     # Shipping rate
     def shipping_rate=(value)
       @shipping_rate = BigDecimal.new(value.to_s)
+    rescue ArgumentError
+      @shipping_rate = 0.0
     end
   end
 end
